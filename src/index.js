debeb2c88263ded00515c8dc1a0f3b2ce075b65f
Sushil Munfan <sushil.m191@gmail.com>
2019-02-20 13:02:28 +0530

    Iterating from objects

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
