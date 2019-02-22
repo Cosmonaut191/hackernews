3004caacf758b7e9d0471fa5b7d84532053fca8f
Sushil Munfan <sushil.m191@gmail.com>
2019-02-22 22:57:41 +0530

    list mapping

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import "./App.css";
 
 const list = [
   {
-    title: "React",
+    title: "React ",
     url: "https://reactjs.org/",
-    author: "Jordan Walke",
+    author: " Jordan Walke",
     num_comments: 3,
     points: 4,
     objectID: 0
@@ -14,7 +14,7 @@ const list = [
   {
     title: "Redux",
     url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
+    author: "  Dan Abramov, Andrew Clark",
     num_comments: 2,
     points: 5,
     objectID: 1
